a8603bae65c2efdc6e1f4c4ab21047c3a7e464d7
Orianne <orianne.essienth@gmail.com>
2025-05-20 15:10:00 +0200

    first try of the 'getDegreeWith' method

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -32,4 +32,46 @@ class Person extends Model
     }
 
 
+    public function findDegree(Person $actual, Person $target,int $degree,  array $visited ){
+
+        if($actual === $target){
+            return $degree;
+        }
+
+        $actualParents=$actual->parents();
+        $actualChildren=$actual->children();
+
+        foreach($actualParents as $actualParent){
+            if(in_array($actualParent,$visited)) continue;
+            array_push($visited,$actualParent);
+            $foundDegree = $this->findDegree($actualParent,$target,$degree+1,$visited);
+            if ($foundDegree !== null) {
+                return $foundDegree;
+            }
+
+        }
+        foreach($actualChildren as $actualChild){
+            if(in_array($actualChild,$visited)) continue;
+            array_push($visited,$actualChild);
+            $foundDegree = $this->findDegree($actualChild,$target,$degree+1,$visited);
+            if ($foundDegree !== null) {
+                return $foundDegree;
+            }
+        }
+
+        return null;
+
+    }
+
+    public function getDegreeWith(Person $target_person_with){
+        $degree=0;
+
+        $visited = [];
+
+
+        return findDegree($this,$target_person_with,0,$visited);
+
+    }
+
+
 }
